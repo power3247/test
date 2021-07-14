1d288a2845dca4dd328ba93e406c7e68ffc66728
doka <posmane5@gmail.com>
2021-07-14 14:47:28 +0900

    map

--- home/views.py
+++ home/views.py
@@ -2,23 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
-
 def index(request):
     path = request.path
     resultstr = ''
     if path == '/home':
         resultstr = '<h1>여기는 home 입니다.</h1>'
-    else:
-        resultstr = '<h1>여기는 김도관 입니다.</h1>'
+    else :
+        resultstr = '<h1>여기는 doka 입니다.</h1>'
 
     return HttpResponse(resultstr)
 
-
 def index01(request):
-    result = {'first': '김도관', 'second': '김도관2'}
+    result = {'first':'doka','second':'kim'}
     return render(request, 'index.html', context=result)
-
 
 def index02(request):
     # result = {'first':request.GET['first'],'second':request.GET['second']}
